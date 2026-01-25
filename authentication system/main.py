e8cb9cb996706ec00c141af6cda7d73621b3d1e9
Aditya <aditya0dagar@gmail.com>
2026-01-25 20:16:49 +0530

    built a basic login form and setup post request to handle the data

--- authentication system/main.py
+++ authentication system/main.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        user_dict = request.form
+        print(user_dict)
+        email = user_dict["email"]
+        password = user_dict["password"]
+        
+        # Add authentication logic here
+        return render_template("login.html",message="Login Successful")
     return render_template("login.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.method == "POST":
+        user_dict = request.form
+        password = user_dict["password"]
+        email = user_dict["email"]
+        
+        # Add registration logic here
+        return redirect(url_for("login"))
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
