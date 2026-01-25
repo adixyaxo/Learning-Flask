fc91f5a1dce395f9fce3bee356ba57f3671abbc1
Aditya <aditya0dagar@gmail.com>
2026-01-25 15:59:00 +0530

    writing marks of students in manual way

--- jinja templating/main.py
+++ jinja templating/main.py
@@ -1,10 +1,17 @@
-from flask import Flask ,render_template, requests
+from flask import Flask ,render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    marks = {
+        "Alice": 85,
+        "Bob": 92,
+        "Charlie": 78,
+        "Diana": 90
+    }
+
+    return render_template("index.html", marks=marks)
 
 if __name__ == "__main__":
     app.run(debug=True)
